6412d6bf07790a276602e661672f38fd3016d2b3
Rob Easthope <hello@robeasthope.com>
2024-03-11 09:20:57 +0000

    Update naming

--- src/components/ui/Box/Box.tsx
+++ src/components/ui/Box/Box.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/utils/tailwind';
 
-const buttonVariants = cva(
+const boxVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
@@ -33,24 +33,24 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+export interface BoxProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof boxVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Box = React.forwardRef<HTMLDivElement, BoxProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(boxVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
     );
   },
 );
-Button.displayName = 'Button';
+Box.displayName = 'Box';
 
-export { Button, buttonVariants };
+export { Box, boxVariants };
