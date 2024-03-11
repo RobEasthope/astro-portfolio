7b54e4ddea16bfc7cd1273c0b730e91419f40154
Rob Easthope <hello@robeasthope.com>
2024-03-11 16:38:50 +0000

    Initial work on updating ProjectIndexCard markup

--- src/components/blocks/ProjectIndex/components/ProjectIndexCard.tsx
+++ src/components/blocks/ProjectIndex/components/ProjectIndexCard.tsx
@@ -1,9 +1,8 @@
-import classNames from 'classnames';
-import type { ProjectProps } from '~/components/work/Project/Project';
 
-import { Box } from '~/components/_base/Box/Box';
-import { InternalLink } from '~/components/_base/InternalLink/InternalLink';
-import { SanityImage } from '~/components/_base/SanityImage/SanityImage';
+import { Box } from '@/components/ui/Box/Box';
+import { InternalLink } from '@/components/ui/InternalLink/InternalLink';
+// import { SanityImage } from '@/components/_base/SanityImage/SanityImage';
+import { cn } from '@/utils/tailwind';
 
 export type ProjectIndexCardProps = {
   project: ProjectProps['page'];
@@ -17,20 +16,20 @@ export function ProjectIndexCard({ project }: ProjectIndexCardProps) {
   return (
     <Box as="li">
       <InternalLink
-        href={project?.slug?.current}
+        href={project?.href}
         className="flex h-full flex-row sm:flex-col items-center gap-0.5 sm:gap-0.25 text-left sm:text-center"
       >
-        <SanityImage
+        {/* <SanityImage
           src={project?.thumbnailImage}
           alt={project?.title}
           aspectRatio={project?.containLogo ? 0 : 1}
-          className={classNames(
+          className={cn(
             'rounded-full w-2 h-2 flex-shrink-0',
             !project?.containLogo && '!object-contain',
           )}
           wrapperClassName="flex items-center"
           cover={!project?.containLogo}
-        />
+        /> */}
 
         {project?.title}
       </InternalLink>
