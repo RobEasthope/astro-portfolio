1ad23168a6b91f36ef16e63d55a0598bdc25eeee
Rob Easthope <hello@robeasthope.com>
2024-03-11 10:44:20 +0000

    Refactor styles check into it's own function

--- src/components/ui/Box/Box.tsx
+++ src/components/ui/Box/Box.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from '@/utils/tailwind';
+import { cn, stylesCheck } from '@/utils/tailwind';
 
 const boxVariants = cva(
   // Base styles
--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function stylesCheck(styles: string | ClassValue[]) {
+  if(!styles) return {};
+
+  return {
+    className: cn(styles)
+  }
+}
