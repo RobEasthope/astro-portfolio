fe9b3d2c26955c697c22d115f3ee905e82d24e5f
Rob Easthope <hello@robeasthope.com>
2024-03-11 15:30:19 +0000

    Add keys for each tech/tool

--- src/content/TOOLS.tsx
+++ src/content/TOOLS.tsx
@@ -1,377 +1,377 @@
 export const TOOLS = {
-  {
+  REDUX: {
   "name": "React Redux",
   "url": "https://react-redux.js.org/"
   },
-  {
+  SHADCN_UI: {
   "name": "Shadcn/ui",
   "url": "https://ui.shadcn.com/"
   },
-  {
+  EJS: {
   "name": "EJS",
   "url": "https://ejs.co/"
   },
-  {
+  YOUTUBE: {
   "name": "Youtube",
   "url": "https://developers.google.com/youtube/"
   },
-  {
+  STORYBOOK: {
   "name": "Storybook",
   "url": "https://storybook.js.org/"
   },
-  {
+  DIRECTUS: {
   "name": "Directus",
   "url": null
   },
-  {
+  NUNJUCKS: {
   "name": "Nunjucks",
   "url": "https://mozilla.github.io/nunjucks/"
   },
-  {
+  JAVASCRIPT: {
   "name": "Javascript",
   "url": "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
   },
-  {
+  GRAPHQL: {
   "name": "GraphQL",
   "url": "https://graphql.org/"
   },
-  {
+  SCALADS: {
   "name": "Scala Digital Signage",
   "url": "https://www.scala.com/"
   },
-  {
+  NODE: {
   "name": "Node.js",
   "url": "https://nodejs.org/en/"
   },
-  {
+  WPML: {
   "name": "WPML",
   "url": "https://wpml.org/"
   },
-  {
+  WEBPACK: {
   "name": "Webpack",
   "url": "https://webpack.js.org/"
   },
-  {
+  VANILLA_EXTRACT: {
   "name": "Vanilla Extract",
   "url": null
   },
-  {
+  STITCHES: {
   "name": "Stitches",
   "url": "https://stitches.dev/"
   },
-  {
+  TAILWIND: {
   "name": "Tailwind",
   "url": "https://tailwindcss.com/"
   },
-  {
+  SANITY: {
   "name": "Sanity",
   "url": "https://www.sanity.io/"
   },
-  {
+  UMBRACO: {
   "name": "Umbraco",
   "url": "https://umbraco.com/"
   },
-  {
+  MAPBOX: {
   "name": "Mapbox",
   "url": "https://www.mapbox.com/"
   },
-  {
+  TRUE_PILL: {
   "name": "TruePill",
   "url": null
   },
-  {
+  "C++" :{
   "name": "C++",
   "url": "https://isocpp.org/"
   },
-  {
+  RADIX_UI: {
   "name": "Radix UI",
   "url": "https://www.radix-ui.com/"
   },
-  {
+  REBASS: {
   "name": "Rebass",
   "url": "https://rebassjs.org/"
   },
-  {
+  HYBRIS: {
   "name": "Hybris",
   "url": "https://www.sap.com/products/crm.html"
   },
-  {
+  CESIUM: {
   "name": "Cesium",
   "url": "https://www.cesium.com/"
   },
-  {
+  ZOD: {
   "name": "Zod",
   "url": "https://zod.dev/"
   },
-  {
+  WORDPRESS: {
   "name": "Wordpress",
   "url": "https://wordpress.org/"
   },
-  {
+  DAY_JS: {
   "name": "Day.js",
   "url": "https://day.js.org/"
   },
-  {
+  SERVERLESS: {
   "name": "Serverless functions",
   "url": "https://aws.amazon.com/lambda/"
   },
-  {
+  PRISMIC: {
   "name": "Prismic",
   "url": "https://prismic.io/"
   },
-  {
+  PHASER_JS: {
   "name": "Phaser.js",
   "url": "https://phaser.io/"
   },
-  {
+  MAILCHIMP: {
   "name": "Mailchimp",
   "url": "https://mailchimp.com/"
   },
-  {
+  MUX: {
   "name": "Mux",
   "url": "https://www.mux.com/"
   },
-  {
+  REACT: {
   "name": "React",
   "url": "https://reactjs.org/"
   },
-  {
+  SEGMENT: {
   "name": "Segment",
   "url": "https://www.segment.io"
   },
-  {
+  SEMBLE: {
   "name": "Semble",
-  "url": null
+  "url": "https://www.semble.io/"
   },
-  {
+  VIMEO: {
   "name": "Vimeo",
   "url": "https://developer.vimeo.com/"
   },
-  {
+  BOOTSTRAP: {
   "name": "Bootstrap",
   "url": "https://www.getbootsrap.com"
   },
-  {
+  TYPEFORM: {
   "name": "Typeform",
   "url": "https://typeform.com"
   },
-  {
+  GULP: {
   "name": "Gulp",
   "url": "https://gulpjs.com/"
   },
-  {
+  HTML: {
   "name": "HTML",
   "url": null
   },
-  {
+  FOUNDATION_FOR_EMAILS: {
   "name": "Foundation for Emails",
   "url": "https://foundation.zurb.com/emails.html"
   },
-  {
+  STYLED_COMPONENTS: {
   "name": "Styled Components",
   "url": "https://styled-components.com/"
   },
-  {
+  ALGOLIA: {
   "name": "Algolia",
   "url": "https://www.algolia.com/"
   },
-  {
+  NETLIFY: {
   "name": "Netlify",
   "url": "https://www.netlify.com/"
   },
-  {
+  ARDUNIO: {
   "name": "Ardunio",
   "url": "https://www.arduino.cc/"
   },
-  {
+  TYPESCRIPT: {
   "name": "Typescript",
   "url": "https://www.typescriptlang.org/"
   },
-  {
+  WATERCOLOUR_PAINT: {
   "name": "Watercolour paint",
   "url": null
   },
-  {
+  FIREBASE: {
   "name": "Firebase",
   "url": "https://firebase.google.com/"
   },
-  {
+  SCSS: {
   "name": "SCSS",
   "url": "https://sass-lang.com/"
   },
-  {
+  FOUNDATION: {
   "name": "Foundation",
   "url": "https://foundation.zurb.com/"
   },
-  {
+  NEXT_JS: {
   "name": "Next.js",
   "url": "https://nextjs.org/"
   },
-  {
+  PUG_JS: {
   "name": "Pug.js",
   "url": "https://pugjs.org/"
   },
-  {
+  PAINT_BRUSHES:{
   "name": "Paint brushes",
   "url": null
   },
-  {
+  SHOPIFY: {
   "name": "Shopify",
   "url": "https://www.shopify.co.uk/"
   },
-  {
+  CANVAS_JS: {
   "name": "Canvas.js",
   "url": "https://canvasjs.com/"
   },
-  {
+  JEST: {
   "name": "Jest",
   "url": null
   },
-  {
+  AUTH0: {
   "name": "Auth0",
   "url": "https://auth0.com/"
   },
-  {
+  PLAYWRIGHT: {
   "name": "Playwright",
   "url": null
   },
-  {
+  CONTENTFUL: {
   "name": "Contentful",
   "url": "https://www.contentful.com/"
   },
-  {
+  IMGIX: {
   "name": "Imgix",
   "url": "https://www.imgix.com/"
   },
-  {
+  REACT_TESTING_LIBRARY: {
   "name": "React Testing Library",
   "url": "https://testing-library.com/"
   },
-  {
+  DRUPAL: {
   "name": "Drupal",
   "url": "https://www.drupal.org/"
   },
-  {
+  CSS: {
   "name": "CSS",
   "url": "https://www.w3.org/Style/CSS/Overview.en.html"
   },
-  {
+  PHOTOSHOP: {
   "name": "Photoshop",
   "url": "https://www.adobe.com/uk/products/photoshop.html"
   },
-  {
+  AWS_S3: {
   "name": "AWS S3",
   "url": "https://aws.amazon.com/s3/"
   },
-  {
+  SITECORE: {
   "name": "Sitecore",
   "url": "https://www.sitecore.com/"
   },
-  {
+  REDUX_TOOLKIT:{
   "name": "Redux Toolkit",
   "url": "https://redux-toolkit.js.org/"
   },
-  {
+  VUE:{
   "name": "Vue",
   "url": "https://vuejs.org/"
   },
-  {
+  FUSION_AUTH:{
   "name": "FusionAuth",
   "url": null
   },
-  {
+  CSS_MODULES: {
   "name": "CSS modules",
   "url": "https://github.com/css-modules/css-modules"
   },
-  {
+  SCROLLMAGIC: {
   "name": "Scrollmagic",
   "url": "https://scrollmagic.io/"
   },
-  {
+  VERCEL: {
   "name": "Vercel",
   "url": "https://vercel.com/"
   },
-  {
+  THREE_JS: {
   "name": "Three.js",
   "url": "https://threejs.org/"
   },
-  {
+  WEB_GL: {
   "name": "WebGL",
   "url": "https://www.khronos.org/webgl/"
   },
-  {
+  ACTION_KIT: {
   "name": "ActionKit",
   "url": "https://www.actionkit.com/"
   },
-  {
+  SQUARESPACE: {
   "name": "SquareSpace",
   "url": "https://www.squarespace.com/"
   },
-  {
+  LARAVEL: {
   "name": "Laravel",
   "url": "https://laravel.com/"
   },
-  {
+  ASTRO: {
   "name": "Astro",
   "url": "https://astro.build/"
   },
-  {
+  GOOGLE_MAPS: {
   "name": "Google Maps",
   "url": "https://developers.google.com/maps/documentation/"
   },
-  {
+  STORYBLOK: {
   "name": "StoryBlok",
   "url": "https://www.storyblok.com/"
   },
-  {
+  REMIX: {
   "name": "Remix",
   "url": "https://remix.run/"
   },
-  {
+  PHP: {
   "name": "PHP",
   "url": "https://www.php.net/"
   },
-  {
+  ONSCHED: {
   "name": "OnSched",
   "url": null
   },
-  {
+  STRIPE: {
   "name": "Stripe",
   "url": "https://stripe.com/gb"
   },
-  {
+  GSAP: {
   "name": "GSAP Animations",
   "url": "https://greensock.com/gsap/"
   },
-  {
+  CYRPRESS: {
   "name": "Cypress",
   "url": null
   },
-  {
+  HUBSPOT: {
   "name": "Hubspot",
   "url": "https://www.hubspot.com/"
   },
-  {
+  FRAMER: {
   "name": "Framer Motion",
   "url": "https://www.framer.com/motion/"
   },
-  {
+  GRUNT: {
   "name": "Grunt",
   "url": "https://gruntjs.com/"
   },
-  {
+  HEROKU: {
   "name": "Heroku",
   "url": "https://www.heroku.com/"
   },
-  {
+  ANGULAR_JS: {
   "name": "AngularJS",
   "url": "https://angular.io/"
   },
-  {
+  BACKBONE_JS:{
   "name": "Backbone.js",
   "url": "https://backbonejs.org/"
   },
-  {
+  GATSBY_JS: {
   "name": "Gatsby.js",
   "url": "https://www.gatsbyjs.org/"
   }
