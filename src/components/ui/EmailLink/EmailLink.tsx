ad048ad0374c99400c5658b5665439c04ac765f6
Rob Easthope <hello@robeasthope.com>
2024-03-11 14:39:26 +0000

    Create EmailLink.tsx

---
+++ src/components/ui/EmailLink/EmailLink.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/utils/tailwind";
+import type React from "react";
+
+// Component props
+export type EmailLinkProps = React.HTMLAttributes<HTMLAnchorElement> & {
+  emailAddress: string;
+  className?: string;
+  children: React.ReactNode;
+};
+
+export function EmailLink({
+  emailAddress,
+  children,
+  className,
+  ...rest
+}: EmailLinkProps) {
+  if (!emailAddress && !children) {
+    return null;
+  }
+
+  return (
+    <a
+      href={`mailto:${emailAddress}`}
+      className={cn(className, 'hover:text-saffron duration-300')}
+      {...rest}
+    >
+      {children}
+    </a>
+  );
+}
