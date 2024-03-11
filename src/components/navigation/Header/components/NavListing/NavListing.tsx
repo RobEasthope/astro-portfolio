cb4474e85044bd9a5240d98d54ef9b9d313e8599
Rob Easthope <hello@robeasthope.com>
2024-03-11 14:42:39 +0000

    Add prop

--- src/components/navigation/Header/components/NavListing/NavListing.tsx
+++ src/components/navigation/Header/components/NavListing/NavListing.tsx
@@ -11,6 +11,7 @@ export function NavListing({ links }: NavListingProps) {
   return <Box as="ul" className="hidden flex-row gap-1 sm:flex">
     {links?.map((link) => (
       <Type as="li" key={uuidv4()} className="text-base">
+        <SuperLink href={link?.href} type={link?.type}>{link?.title}</SuperLink>
       </Type>
     ))}
   </Box>
