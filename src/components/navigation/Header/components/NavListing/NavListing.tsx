1693b41b3837c36c0b19f2b0a71d358684dd1eeb
Rob Easthope <hello@robeasthope.com>
2024-03-11 14:42:20 +0000

    Refactor props

--- src/components/navigation/Header/components/NavListing/NavListing.tsx
+++ src/components/navigation/Header/components/NavListing/NavListing.tsx
@@ -3,14 +3,13 @@ import { Type } from '@/components/ui/Type/Type';
 import { v4 as uuidv4 } from "uuid";
 
 export type NavListingProps = {
-  links: {title: string, href: string}[];
+  links: SuperLinkProps[];
 };
 
 export function NavListing({ links }: NavListingProps) {
   return <Box as="ul" className="hidden flex-row gap-1 sm:flex">
     {links?.map((link) => (
       <Type as="li" key={uuidv4()} className="text-base">
-        <SuperLink link={link?.href}>{link?.title}</SuperLink>
       </Type>
     ))}
   </Box>
