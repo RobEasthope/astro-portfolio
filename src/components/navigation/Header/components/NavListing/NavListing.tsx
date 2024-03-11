3d22badbfa31b3be34b3d01ee4985ee30f135448
Rob Easthope <hello@robeasthope.com>
2024-03-11 14:26:51 +0000

    NavListing code formatting

--- src/components/navigation/Header/components/NavListing/NavListing.tsx
+++ src/components/navigation/Header/components/NavListing/NavListing.tsx
@@ -15,12 +15,12 @@ export type NavListingProps = {
     | null;
 };
 
-export const NavListing = ({ links }: NavListingProps) => (
-  <Box as="ul" className="hidden flex-row gap-1 sm:flex">
+export function NavListing({ links }: NavListingProps) {
+  return <Box as="ul" className="hidden flex-row gap-1 sm:flex">
     {links?.map((link) => (
       <Type as="li" key={link?._key} className="text-base">
         <SuperLink link={link}>{link.title}</SuperLink>
       </Type>
     ))}
   </Box>
-);
+}
