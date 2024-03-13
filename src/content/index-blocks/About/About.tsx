5db2bd7c9aaf618b6ae3665de3b60dedbdb81db7
Rob Easthope <hello@robeasthope.com>
2024-03-13 11:49:19 +0000

    Revert "Move archived project content to hidden pages folder"
    This reverts commit c43294be6282ee32893ee6cde233ea0c144fde56.

---
+++ src/content/index-blocks/About/About.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@/components/ui/Box/Box";
+
+export function About() {
+  return (
+    <Box as="section">
+      <h1>About</h1>
+      <p>This is the About section</p>
+    </Box>
+  );
+}
