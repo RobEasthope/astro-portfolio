1e4f61f4430985d57e35ae991296a536ce0d737d
Rob Easthope <hello@robeasthope.com>
2024-03-15 18:42:38 +0000

    Use WorkSchemaZProps in collection config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,11 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+import { WorkSchemaZProps } from "@/schemas/work.schema";
 
 // 2. Define your collection(s)
 const workCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    agency: z.string().optional(),
-    agencyUrl: z.string().url().optional(),
-    client: z.string().optional(),
-    clientUrl: z.string().url().optional(),
-    projectUrlTitle: z.string().optional(),
-    projectUrl: z.string().url().optional(),
-    repoTitle: z.string().optional(),
-    repoUrl: z.string().url().optional(),
-    tools: z.string().optional(),
-    dates: z.string(),
-  }),
+  schema: WorkSchemaZProps,
 });
 
 // 3. Export a single `collections` object to register your collection(s)
