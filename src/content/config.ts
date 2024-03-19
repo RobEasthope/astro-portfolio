10a720cdd628d916a13687c21d0fcc132c308c2c
Rob Easthope <hello@robeasthope.com>
2024-03-19 18:02:57 +0000

    Refactor Work schema zod typings

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,14 @@
 // 1. Import utilities from `astro:content`
 // eslint-disable-next-line import/no-unresolved
 import { defineCollection, z } from "astro:content";
-import { WorkSchemaZProps } from "@/schemas/work.schema";
+import { WorkSchemaDataProps } from "@/schemas/work.schema";
 
 // 2. Define your collection(s)
 const workCollection = defineCollection({
   type: "content", // v2.5.0 and later
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      description: z.string(),
-      url: z.string(),
-      agency: z.string().optional(),
-      agencyUrl: z.string().url().optional(),
-      client: z.string().optional(),
-      clientUrl: z.string().url().optional(),
-      projectUrlTitle: z.string().optional(),
-      projectUrl: z.string().url().optional(),
-      repoTitle: z.string().optional(),
-      repoUrl: z.string().url().optional(),
-      tools: z.string().optional(),
-      dates: z.string(),
+      ...WorkSchemaDataProps,
       thumbnailImage: image()
         .refine((img) => img.width >= 64, {
           message: "Cover image must be at least 64 pixels wide!",
