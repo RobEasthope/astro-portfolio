7a207ffdec8e5243262166889a772a43d6766e90
Rob Easthope <hello@robeasthope.com>
2024-03-19 17:25:18 +0000

    Render image by using image helper

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,32 @@
 // 1. Import utilities from `astro:content`
 // eslint-disable-next-line import/no-unresolved
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { WorkSchemaZProps } from "@/schemas/work.schema";
 
 // 2. Define your collection(s)
 const workCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: WorkSchemaZProps,
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      url: z.string(),
+      agency: z.string().optional(),
+      agencyUrl: z.string().url().optional(),
+      client: z.string().optional(),
+      clientUrl: z.string().url().optional(),
+      projectUrlTitle: z.string().optional(),
+      projectUrl: z.string().url().optional(),
+      repoTitle: z.string().optional(),
+      repoUrl: z.string().url().optional(),
+      tools: z.string().optional(),
+      dates: z.string(),
+      thumbnailImage: image()
+        .refine((img) => img.width >= 64, {
+          message: "Cover image must be at least 64 pixels wide!",
+        })
+        .optional(),
+    }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
