4614847250fb0b5d864ace4a226a01389a8c26a4
Rob Easthope <hello@robeasthope.com>
2024-03-21 09:54:50 +0000

    Centre dot

--- src/components/decoration/Dot/Dot.tsx
+++ src/components/decoration/Dot/Dot.tsx
@@ -18,7 +18,7 @@ export function Dot({
   return (
     <div
       className={cn(
-        `flex h-1 w-full items-center text-lg text-ink`,
+        `flex h-1 w-full items-center justify-center text-lg text-ink`,
         `mt-${spacingTop}`,
         `mb-${spacingBottom}`,
         "print:my-0.5",
