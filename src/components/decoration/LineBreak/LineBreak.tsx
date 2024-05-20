61d1bd7255e9119f332b26c2d95617d0e52f5790
Rob Easthope <hello@robeasthope.com>
2024-05-20 15:15:22 +0100

    Formatting

--- src/components/decoration/LineBreak/LineBreak.tsx
+++ src/components/decoration/LineBreak/LineBreak.tsx
@@ -8,7 +8,7 @@ export function LineBreak({ className }: LineBreakProps) {
   return (
     <hr
       className={cn(
-        'after:bg-dark-saffron flex h-1 items-center justify-center border-none after:block after:h-[2px] after:w-3  after:content-[""]',
+        'flex h-1 items-center justify-center border-none after:block after:h-[2px] after:w-3 after:bg-dark-saffron  after:content-[""]',
         className,
       )}
     />
