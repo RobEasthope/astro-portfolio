9a2b3253ce78d06a7bda4b1a286c9b84b484002c
Rob Easthope <hello@robeasthope.com>
2024-05-20 17:41:37 +0100

    Change work section layout

--- src/content/WorkSection.tsx
+++ src/content/WorkSection.tsx
@@ -10,21 +10,22 @@ const projects = await getCollection("work");
 
 export function WorkSection() {
   return (
-    <Box as="section" id="work" className="mx-auto w-full max-w-prose">
-      <Type as="h1" className="mb-0.5 text-2xl leading-tight">
-        Work
-      </Type>
+    <Box as="section" id="work" className="mx-auto w-full max-w-40">
+      <Box as="section" id="work" className="mx-auto max-w-prose">
+        <Type as="h1" className="mb-0.5 text-center text-2xl leading-tight">
+          Work
+        </Type>
 
-      <Type as="p" className="mb-1 text-md italic">
-        Some of the projects I have worked on
-      </Type>
+        <Type as="p" className="mb-1 text-center text-md italic">
+          Some of the projects I have worked on
+        </Type>
 
-      <LineBreak className="my-1" align="left" />
+        <LineBreak className="my-1" align="centre" />
+      </Box>
 
       <Box
         as="ul"
-        className="mx-auto grid grid-cols-1 gap-x-1 gap-y-0.5 sm:grid-cols-2 md:grid-cols-2 md:gap-x-2 md:gap-y-1"
-        maxWidth="medium"
+        className="mx-auto grid grid-cols-1 gap-x-1 gap-y-0.5 sm:grid-cols-2 md:grid-cols-3 md:gap-x-2 md:gap-y-1 lg:grid-cols-3"
       >
         {projects?.map((project) => (
           <Type as="li">
