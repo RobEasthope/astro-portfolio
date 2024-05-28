590a3f12d96cf084ffc1bde5b8d1e238c1604cec
Rob Easthope <hello@robeasthope.com>
2024-05-28 14:51:53 +0100

    Add missing key

--- src/content/WorkSection.tsx
+++ src/content/WorkSection.tsx
@@ -5,6 +5,7 @@ import { LineBreak } from "@/components/decoration/LineBreak/LineBreak";
 import { Box } from "@/components/ui/Box/Box";
 import { InternalLink } from "@/components/ui/InternalLink/InternalLink";
 import { Type } from "@/components/ui/Type/Type";
+import { v4 as uuidv4 } from "uuid";
 
 const listedProjects = await getCollection("listed-projects");
 const listedProjectsByDate = listedProjects.sort((a, b) => {
@@ -33,7 +34,7 @@ export function WorkSection() {
         className="mx-auto grid w-full max-w-5xl grid-cols-1 gap-x-1 gap-y-0.5 sm:grid-cols-2 md:grid-cols-3 md:gap-x-2 md:gap-y-1 lg:grid-cols-4"
       >
         {listedProjectsByDate?.map((project) => (
-          <Type as="li">
+          <Type key={uuidv4()} as="li">
             <InternalLink
               href={project.data.url}
               className="flex h-full flex-row gap-0.5"
