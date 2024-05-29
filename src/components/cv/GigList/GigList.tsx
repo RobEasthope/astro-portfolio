0b8e8398144d10896a9d1a047e4e3e75da5b7cce
Rob Easthope <hello@robeasthope.com>
2024-05-29 12:38:47 +0100

    Pass in data correctly

--- src/components/cv/GigList/GigList.tsx
+++ src/components/cv/GigList/GigList.tsx
@@ -1,27 +1,30 @@
-import Gig from "@/components/cv/Gig/Gig.astro";
+import { Gig } from "@/components/cv/Gig/Gig";
 import { Box } from "@/components/ui/Box/Box";
 import type { GigSchemaProps } from "@/schemas/gig.schema";
 import type { z } from "astro:content";
 import { v4 as uuidv4 } from "uuid";
 
 export type GigListProps = {
-  gigs: z.infer<typeof GigSchemaProps>[];
+  gigs: { data: z.infer<typeof GigSchemaProps>[] };
 };
 
 export function GigList({ gigs }: GigListProps) {
   if (!gigs?.length) return null;
+
+  // Pick off main content from the entry
 
   return (
     <Box as="ul" className="flex flex-col gap-1.5 print:gap-0.5">
       {gigs?.map((gig) => (
         <Gig
           key={uuidv4()}
-          employeer={gig.employer}
-          employeerUrl={gig.employerUrl}
-          jobTitle={gig.jobTitle}
-          jobType={gig.jobType}
-          startDate={gig.startDate}
-          endDate={gig.endDate}
+          employer={gig?.data?.employer}
+          entry={gig.body}
+          employeerUrl={gig?.data?.employerUrl}
+          jobTitle={gig?.data?.jobTitle}
+          jobType={gig?.data?.jobType}
+          startDate={gig?.data?.startDate}
+          endDate={gig?.data?.endDate}
         />
       ))}
     </Box>
