1e26cdd5c3fd63fa4bc8af3a7bff24951cbf52b9
Rob Easthope <hello@robeasthope.com>
2024-05-29 12:38:32 +0100

    Convert back to .tsx format

---
+++ src/components/cv/Gig/Gig.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@/components/ui/Box/Box";
+import { Type } from "@/components/ui/Type/Type";
+import { GigDate } from "@/components/cv/GigDate/GigDate";
+import type { GigSchemaProps } from "@/schemas/gig.schema";
+import type { z } from "astro:content";
+
+export function Gig({
+  employer,
+  jobTitle,
+  startDate,
+  endDate,
+}: z.infer<typeof GigSchemaProps>) {
+  return (
+    <Box as="article">
+      <Type as="h4" className="font-bold">
+        {employer} / {jobTitle}
+      </Type>
+
+      <GigDate startDate={startDate} endDate={endDate} />
+
+      {/* <Content /> */}
+    </Box>
+  );
+}
