27381f162a214d9431a5cd2bc09fc2d8c4777021
Rob Easthope <hello@robeasthope.com>
2024-05-29 08:34:49 +0100

    Rename work collection to match

--- src/content/config.ts
+++ src/content/config.ts
@@ -29,6 +29,6 @@ const gigCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  "listed-work": workCollection,
+  work: workCollection,
   gigs: gigCollection,
 };
