47bf2131ca44da56c808ac5a73f243c984f1bb84
Rob Easthope <hello@robeasthope.com>
2024-05-29 11:56:48 +0100

    GigList formatting

--- src/components/cv/GigList/GigList.tsx
+++ src/components/cv/GigList/GigList.tsx
@@ -1,20 +1,20 @@
-import { Box } from '~/components/_base/Box/Box';
+import { Box } from "~/components/_base/Box/Box";
 
-import type { GigProps } from '~/components/about/GigIndex/components/Gig/Gig';
-import { Gig } from '~/components/about/GigIndex/components/Gig/Gig';
+import type { GigProps } from "~/components/about/GigIndex/components/Gig/Gig";
+import { Gig } from "~/components/about/GigIndex/components/Gig/Gig";
 
 export type GigListProps = {
   gigs: GigProps[];
 };
 
-export const GigList = ({ gigs }: GigListProps) => {
+export function GigList({ gigs }: GigListProps) {
   if (!gigs?.length) return null;
 
   return (
     <Box as="ul" className="flex flex-col gap-1.5 print:gap-0.5">
       {gigs?.map((gig, i) => (
         <Gig
-          key={`${gig?._id || ''}-${i}`}
+          key={`${gig?._id || ""}-${i}`}
           client={gig.client}
           jobTitle={gig.jobTitle}
           description={gig.description}
@@ -24,4 +24,4 @@ export const GigList = ({ gigs }: GigListProps) => {
       ))}
     </Box>
   );
-};
+}
