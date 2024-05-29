e30fd5b48ebe02bbc60b024a9c6627b30867c31f
Rob Easthope <hello@robeasthope.com>
2024-05-29 12:01:16 +0100

    Use uuid key

--- src/components/cv/GigList/GigList.tsx
+++ src/components/cv/GigList/GigList.tsx
@@ -2,6 +2,7 @@ import Gig from "@/components/cv/Gig/Gig.astro";
 import { Box } from "@/components/ui/Box/Box";
 import type { GigSchemaProps } from "@/schemas/gig.schema";
 import type { z } from "astro:content";
+import { v4 as uuidv4 } from "uuid";
 
 export type GigListProps = {
   gigs: z.infer<typeof GigSchemaProps>[];
@@ -12,10 +13,10 @@ export function GigList({ gigs }: GigListProps) {
 
   return (
     <Box as="ul" className="flex flex-col gap-1.5 print:gap-0.5">
-      {gigs?.map((gig, i) => (
+      {gigs?.map((gig) => (
         <Gig
-          key={`${gig?._id || ""}-${i}`}
           client={gig.client}
+          key={uuidv4()}
           jobTitle={gig.jobTitle}
           description={gig.description}
           startDate={gig.startDate}
