a4dac2354f8bf29f56cb3094b807c79d57434836
Rob Easthope <hello@robeasthope.com>
2024-05-30 11:27:51 +0100

    Remove sabon font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,7 +69,6 @@ export default {
       fontFamily: {
         sans: ["aktiv-grotesk", ...defaultTheme.fontFamily.sans],
         plantin: ["plantin", ...defaultTheme.fontFamily.serif],
-        sabon: ["linotype-sabon", ...defaultTheme.fontFamily.serif],
       },
       fontSize: {
         xs: fluidTypeSize({
