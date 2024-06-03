df0749db129f41f99fbd6b95331f06dfedf346ad
Rob Easthope <hello@robeasthope.com>
2024-06-03 10:09:38 +0100

    Remove w-full from Dot and Diamond

--- src/components/decoration/Dot/Dot.tsx
+++ src/components/decoration/Dot/Dot.tsx
@@ -18,7 +18,7 @@ export function Dot({
   return (
     <div
       className={cn(
-        `flex h-1 w-full items-center justify-center text-lg text-decoration`,
+        `flex h-1 items-center justify-center text-lg text-decoration`,
         `mt-${spacingTop}`,
         `mb-${spacingBottom}`,
         "print:my-0.5",
