88e40b91d9a4e49515b5710a1b301da0d8cf860c
Rob Easthope <hello@robeasthope.com>
2024-06-03 22:19:06 +0100

    Update layout for smaller sizes

--- src/components/container/LandingPageSection/LandingPageSection.tsx
+++ src/components/container/LandingPageSection/LandingPageSection.tsx
@@ -17,9 +17,12 @@ export function LandingPageSection({
     <Box as="section" id={id} className="w-full">
       <Box
         as="div"
-        className="mx-auto flex max-w-7xl flex-col gap-1 md:flex-row md:gap-2"
+        className="mx-auto flex max-w-prose flex-col justify-center gap-1 md:max-w-7xl md:flex-row md:gap-2"
       >
-        <Type as="h2" className="tracking-none text-center text-2xl md:w-1/3">
+        <Type
+          as="h2"
+          className="tracking-none text-center text-2xl sm:text-left md:w-1/3"
+        >
           {heading}
         </Type>
         <Box as="div" className="md:w-2/3">
