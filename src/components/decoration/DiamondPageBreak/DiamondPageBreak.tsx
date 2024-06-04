f2a84aae142eeb724bc05d59ce4cdceb3c25fc52
Rob Easthope <hello@robeasthope.com>
2024-06-04 11:12:10 +0100

    Tweak colours

--- src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
+++ src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/utils/tailwind";
 import { Diamond } from "@/components/decoration/Diamond/Diamond";
 
 const DiamondPageBreak = React.forwardRef<
@@ -16,11 +15,11 @@ const DiamondPageBreak = React.forwardRef<
       ref={ref}
       className="mx-auto flex w-full flex-row items-center gap-0.5"
     >
-      <div className="h-[1px] w-1/2 bg-ink" />
+      <div className="h-[1px] w-1/2 bg-gray-6" />
 
       <Diamond />
 
-      <div className="h-[1px] w-1/2 bg-ink" />
+      <div className="h-[1px] w-1/2 bg-gray-6" />
     </div>
   ),
 );
