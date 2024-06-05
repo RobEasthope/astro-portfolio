eecac60cce4357d4e30c50313dabcacd80bf4a2c
Rob Easthope <hello@robeasthope.com>
2024-06-05 20:28:39 +0100

    Create wide-content spacing var

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -160,6 +160,9 @@ export default {
         56: `calc(${BASE_UNIT} * 56)`,
         60: `calc(${BASE_UNIT} * 60)`,
         64: `calc(${BASE_UNIT} * 64)`,
+
+        // UI spacing
+        "wide-content": "calc(44ch+65ch+2rem)",
       },
       borderRadius: {
         lg: "var(--radius)",
