7cb241c52eff84c307ba7ee0c54f0ff550eca8e4
Rob Easthope <hello@robeasthope.com>
2024-06-06 10:08:19 +0100

    Set ul marker style via TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -184,6 +184,15 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            "ul > li::marker": {
+              color: "rgb(21, 23, 24)",
+            },
+          },
+        },
+      },
     },
   },
   plugins: [tailwindTypography, tailwindcssRadixColors],
