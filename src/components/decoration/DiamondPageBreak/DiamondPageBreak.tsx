857680eec6e62ad0360b536193a018b84413daf2
Rob Easthope <hello@robeasthope.com>
2024-06-11 18:41:11 +0100

    Integrate className prop

--- src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
+++ src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
@@ -2,11 +2,20 @@ import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 import { Diamond } from "@/components/decoration/Diamond/Diamond";
+import { cn } from "@/utils/tailwind";
 
 const DiamondPageBreak = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(({ className }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "mx-auto flex w-full flex-row items-center gap-0.5",
+      className,
+    )}
+  >
+    <div className="h-[1px] w-1/2 bg-gray-6" />
 
     <Diamond />
 
