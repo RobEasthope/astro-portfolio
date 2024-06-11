53e3d656eef56ac2565cb9e396e2d41cba141678
Rob Easthope <hello@robeasthope.com>
2024-06-11 18:16:21 +0100

    Quick install of astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), mdx()]
+  }), mdx(), robotsTxt()]
 });
