b386f19e441c68290746908d0428e46172861ca4
Rob Easthope <hello@robeasthope.com>
2024-06-11 18:40:48 +0100

    Clean up props

--- src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
+++ src/components/decoration/DiamondPageBreak/DiamondPageBreak.tsx
@@ -6,23 +6,13 @@ import { Diamond } from "@/components/decoration/Diamond/Diamond";
 const DiamondPageBreak = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref,
-  ) => (
-    <div
-      ref={ref}
-      className="mx-auto flex w-full flex-row items-center gap-0.5"
-    >
-      <div className="h-[1px] w-1/2 bg-gray-6" />
+>(({ className }, ref) => (
 
-      <Diamond />
+    <Diamond />
 
-      <div className="h-[1px] w-1/2 bg-gray-6" />
-    </div>
-  ),
-);
+    <div className="h-[1px] w-1/2 bg-gray-6" />
+  </div>
+));
 DiamondPageBreak.displayName = SeparatorPrimitive.Root.displayName;
 
 export { DiamondPageBreak };
