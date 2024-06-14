8bfd2f9fe3a2640e9247e6f75c713179e13dfda8
Rob Easthope <hello@robeasthope.com>
2024-06-14 10:17:53 +0100

    Add MuxPlayer

--- src/components/blocks/MuxVideo/MuxVideo.tsx
+++ src/components/blocks/MuxVideo/MuxVideo.tsx
@@ -1,11 +1,22 @@
-import AstroImage from "@/components/_base/AstroImage/AstroImage.astro";
 import { Box } from "@/components/_base/Box/Box";
 import { Type } from "@/components/_base/Type/Type";
 
 export function MuxVideo({ videoID, caption }: { videoID: string, caption: string }) {
   return (<Box as="section" breakout blockSpacing>
   <Box as="figure" className="mx-auto" maxWidth={maxWidth}>
-    <AstroImage src={image} alt={caption} className="mx-auto my-0" />
+    <MuxPlayer
+    playbackId="9VeINC4f1SC23jNX01VDhxV6rmwI3DTz01"
+    metadata={{
+      video_id: "Yytp7lXoNk1ndRc8a01TK3nbn02XdNmhq7",
+      video_title: "Flight off Great End",
+      viewer_user_id: "user-id-007",
+    }}
+    streamType="on-demand"
+    loop
+    autoPlay
+    muted
+    client:load
+  />
     <Box as="div" className="mx-auto sm:max-w-lg lg:max-w-5xl">
       <Type as="figcaption" className="text-gray-600 text-sm italic ">
         {caption}
